575287e11fa4f755ffe451c2d7e3fbba787516f5
Daryl Buckle <darylbuckle1@gmail.com>
2020-12-11 23:44:03 +0000

    Updates

---
+++ precache-manifest.a141961f60cfe01fdb891cb12556a3cc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b37c9ba2b34f4ce128226683d086f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6ae7d8e038d23153c2e",
+    "url": "./static/css/2.e9128ce8.chunk.css"
+  },
+  {
+    "revision": "eaa4bf3e751343281302",
+    "url": "./static/css/main.ccc8e58d.chunk.css"
+  },
+  {
+    "revision": "b6ae7d8e038d23153c2e",
+    "url": "./static/js/2.2de9ea76.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2de9ea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa4bf3e751343281302",
+    "url": "./static/js/main.75d1ea86.chunk.js"
+  },
+  {
+    "revision": "e727a828d22589c485b5",
+    "url": "./static/js/runtime-main.2b87e3b6.js"
+  },
+  {
+    "revision": "e47ddbb27bfa8bfba866ea558d8482fe",
+    "url": "./static/media/apple.e47ddbb2.jpeg"
+  },
+  {
+    "revision": "28d0366c9cf3229216afae0b161593bc",
+    "url": "./static/media/applepie.28d0366c.jpeg"
+  },
+  {
+    "revision": "5f723924b90ffca49bf569127270104e",
+    "url": "./static/media/background.5f723924.jpeg"
+  },
+  {
+    "revision": "90039b21b331649b81b75943861409b0",
+    "url": "./static/media/keesewing.90039b21.jpeg"
+  },
+  {
+    "revision": "fd0e8e0165ef1bf72bee5a85eeab6539",
+    "url": "./static/media/rice.fd0e8e01.jpeg"
+  },
+  {
+    "revision": "69c6f39c8563b2455fa22d44f483ace3",
+    "url": "./static/media/shroom.69c6f39c.jpeg"
+  },
+  {
+    "revision": "0d867f57d8564d97cc35e61c89af6b22",
+    "url": "./static/media/toenail.0d867f57.jpeg"
+  },
+  {
+    "revision": "3f3c38bac9441c5b3870b4b3dd229c2b",
+    "url": "./static/media/truffle.3f3c38ba.jpeg"
+  }
+]);
